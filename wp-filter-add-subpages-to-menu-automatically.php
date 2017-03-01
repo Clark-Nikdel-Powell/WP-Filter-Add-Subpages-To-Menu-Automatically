5b60ea0f624e669a2526fb2e0f9800c6b4acc533
Josh Nederveld <joshnederveld@gmail.com>
2017-03-01 11:15:37 -0500

    Need to use wp_list_pages here to get grandchildren as lists

--- wp-filter-add-subpages-to-menu-automatically.php
+++ wp-filter-add-subpages-to-menu-automatically.php
@@ -4,60 +4,45 @@ namespace CNP;
 /**
  * Filter in subpages to a menu automatically if add_subpages_automatically is set and true on the menu args.
  *
- * @param $items
+ * @param $item_output
+ * @param $item
+ * @param $depth
  * @param $args
  *
- * @return array
+ * @return string
  */
-function filter_in_sub_pages( $items, $args ) {
+function filter_in_sub_pages( $item_output, $item, $depth, $args ) {
 
 	if ( ! isset( $args->add_subpages_automatically ) || false === $args->add_subpages_automatically ) {
-		return $items;
+		return $item_output;
 	}
 
-	$current_post = get_post();
-
-	$tmp = array();
-	foreach ( $items as $key => $i ) {
-
-		$tmp[] = $i;
-
-		//if not page move on
-		if ( 'page' !== $i->object ) {
-			continue;
-		}
-
-		$page = get_post( $i->object_id );
-		//if not parent page move on
-		if ( ! isset( $page->post_parent ) || 0 !== $page->post_parent ) {
-			continue;
-		}
-
-		$children = get_pages( array( 'child_of' => $i->object_id ) );
-
-		foreach ( (array) $children as $c ) {
-
-			//set parent menu
-			$c->menu_item_parent      = $i->ID;
-			$c->object_id             = $c->ID;
-			$c->object                = 'page';
-			$c->type                  = 'post_type';
-			$c->type_label            = 'Page';
-			$c->url                   = get_permalink( $c->ID );
-			$c->title                 = $c->post_title;
-			$c->target                = '';
-			$c->attr_title            = '';
-			$c->description           = '';
-			$c->classes               = array( '', 'menu-item', 'menu-item-type-post_type', 'menu-item-object-page' );
-			$c->xfn                   = '';
-			$c->current               = ( $current_post->ID == $c->ID ) ? true : false;
-			$c->current_item_ancestor = ( $current_post->ID == $c->post_parent ) ? true : false;
-			$c->current_item_parent   = ( $current_post->ID == $c->post_parent ) ? true : false;
-			$tmp[]                    = $c;
-		}
+	// if not page move on.
+	if ( 'page' !== $item->object ) {
+		return $item_output;
 	}
 
-	return $tmp;
+	$page = get_post( $item->object_id );
+	// if not parent page move on.
+	if ( ! isset( $page->post_parent ) || 0 !== $page->post_parent ) {
+		return $item_output;
+	}
+
+	$pages_args = [
+		'sort_column' => 'menu_order',
+		'child_of'    => $item->object_id,
+		'title_li'    => '',
+		'echo'        => false,
+		'context'     => 'menu',
+		'walker'      => new Filterable_Walker_Page(),
+	];
+
+	$children = wp_list_pages( $pages_args );
+	if ( '' !== $children ) {
+		$item_output .= '<ul class="sub-menu">' . $children . '</ul>';
+	}
+
+	return $item_output;
 }
 
-add_filter( 'wp_nav_menu_objects', 'CNP\filter_in_sub_pages', 20, 2 );
+add_filter( 'walker_nav_menu_start_el', 'CNP\filter_in_sub_pages', 20, 4 );
